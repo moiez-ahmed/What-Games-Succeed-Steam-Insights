3c633869fb693108bc5f4defb852dd3d376546fc
Moiez <moiezahmed38@gmail.com>
2025-08-05 23:30:23 +0500

    Minor Changes

---
+++ app.py
@@ -0,0 +1,11 @@
+import requests
+import pandas as pd
+
+url = "https://steamspy.com/api.php?request=all"
+response = requests.get(url)
+
+data = response.json()
+df = pd.DataFrame(data.values())
+
+print(df.head())
+print(df.info())
